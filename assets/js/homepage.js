6e4268d70f8c17057c9357d503de0638d294fd2e
Ryan Skog <ryan.skog@outlook.com>
2021-01-08 11:18:30 -0800

    Created function to dynamically retrieve user repos

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,18 @@
-const getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+const getUserRepos = function (user) {
+    // Fetch is async, so this won't work:
+    // let response = fetch("https://api.github.com/users/octocat/repos");
+    // console.log(response);
+
+    let apiUrl = `https://api.github.com/users/${user}/repos`;
+
+    fetch(apiUrl).then(function (response) {
+        // The response has a method called json() whose callback
+        // function captures the actual data.
+        console.log(response);
+        response.json().then(function (data) {
+            console.log(data);
+        })
+    })
 };
 
-getUserRepos();
+getUserRepos("raskog1");
