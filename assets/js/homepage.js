9736bf77b48517f8bea2b172569420624ed8416c
Ryan Skog <ryan.skog@outlook.com>
2021-01-15 12:47:24 -0800

    Fetching api data, dynamically feeding into HTML

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,20 @@
+const userFormEl = document.querySelector("#user-form");
+const nameInputEl = document.querySelector("#username");
+const repoContainerEl = document.querySelector("#repos-container");
+const repoSearchTerm = document.querySelector("#repo-search-term");
+
+const formSubmitHandler = function (event) {
+    event.preventDefault();
+    let username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+}
+
 const getUserRepos = function (user) {
     // Fetch is async, so this won't work:
     // let response = fetch("https://api.github.com/users/octocat/repos");
@@ -9,10 +26,63 @@ const getUserRepos = function (user) {
         // The response has a method called json() whose callback
         // function captures the actual data.
         console.log(response);
-        response.json().then(function (data) {
-            console.log(data);
-        })
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data, user);
+            })
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    }).catch(function (error) {
+        // Notice this '.catch()' getting chained onto the end of '.then()'
+        alert("Unable to connect to Github");
     })
 };
 
-getUserRepos("raskog1");
+const displayRepos = function (repos, searchTerm) {
+    // Check if api returned any repos
+    if (repos.length === 0) {
+        repoContainerEl.textContent = "No repositories found.";
+        return;
+    }
+
+    // Clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // Loop over repos
+    for (let i = 0; i < repos.length; i++) {
+        // Format repo name
+        const repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // Create a container for each repo
+        const repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // Create a span element to hold repository name
+        const titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // Append to container
+        repoEl.appendChild(titleEl);
+
+        // Create a status element
+        const statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // Check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // Append to container
+        repoEl.appendChild(statusEl);
+
+        // Append container to the dom
+        repoContainerEl.appendChild(repoEl);
+    }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
